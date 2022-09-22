1add1954886fee4987888070e9cd57c3ea777bf0
Kimutai01 <kiprotichkimutai01@gmail.com>
2022-09-22 20:08:32 +0300

    Added changes

--- src/components/BabyHog.js
+++ src/components/BabyHog.js
@@ -1,13 +1,13 @@
 import React from "react";
 import normalBaby from "../assets/unadulterated-hoglette.png";
-import SunBaby from "../assets/sun-eyes.png";
-import BlueBaby from "../assets/blue-eyes.png";
-import GlowingBaby from "../assets/glowing-eyes.png";
+// import SunBaby from "../assets/sun-eyes.png";
+// import BlueBaby from "../assets/blue-eyes.png";
+// import GlowingBaby from "../assets/glowing-eyes.png";
 
 // is there a way we could associate eye color string values with images?
 // perhaps so we could do something along the lines of `eyeColorMapper['blue'] and get back the right image?`
 
-function BabyHog(props) {
+function BabyHog({ name, hobby, eyeColor }) {
   function handleChangeWeight(e) {
     // how can we reuse this for both buttons?
     // perhaps something with e.target.name === "+"
@@ -15,10 +15,11 @@ function BabyHog(props) {
 
   return (
     <li className="hogbabies">
-      <h1>Name</h1>
+      <img src="" alt="" />
+      <h1>Name:{name}</h1>
       <h3>Weight:</h3>
-      <h3>Hobby:</h3>
-      <h4>Eye Color:</h4>
+      <h3>Hobby:{hobby}</h3>
+      <h4>Eye Color:{eyeColor}</h4>
 
       <button name="+">Increase Weight</button>
       <button name="-">Decrease Weight</button>
